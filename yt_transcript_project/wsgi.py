9fadd9ce8e0abe689b5744e3ec9f1f01d0013c65
ambuj4 <ambuj@blueribbonmedia.com>
2025-03-07 18:12:46 +0530

    Updated WSGI for Vercel deployment

--- yt_transcript_project/wsgi.py
+++ yt_transcript_project/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yt_transcript_project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yt_transcript_project.settings")
 
-app = get_wsgi_application()
+app = get_wsgi_application()  # ✅ Vercel expects 'app' to be defined
