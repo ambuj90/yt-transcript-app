fd1ac033e3a87c6e099c19ec58ab95df4bba3ad5
ambuj4 <ambuj@blueribbonmedia.com>
2025-03-07 21:22:22 +0530

    Initial commit

--- yt_transcript_project/settings.py
+++ yt_transcript_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "django-insecure-3ic7#w@iol29y_z_v_v02l-5tv4u6)_qfxi_u8iuk3!ioew=dw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['youtube-ambuj-yt.onrender.com', '127.0.0.1', 'localhost']
+
+
 
 
 
@@ -82,11 +84,10 @@ WSGI_APPLICATION = "yt_transcript_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
