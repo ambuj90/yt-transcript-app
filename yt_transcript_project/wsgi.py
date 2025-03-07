cdd9fd3d7e0c6eef0e8d37f97d0dbcf45ca0efa3
ambuj4 <ambuj@blueribbonmedia.com>
2025-03-07 18:24:36 +0530

    Fixed WSGI config

--- yt_transcript_project/wsgi.py
+++ yt_transcript_project/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yt_transcript_project.settings")
 
-app = get_wsgi_application()  # ✅ Vercel expects 'app' to be defined
+app = get_wsgi_application()  # ✅ Vercel expects 'app' as the callable object
